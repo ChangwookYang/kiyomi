17769a729ffaed2341a6f551b9935a98a8b6e297
양창욱(Changwook Yang)/Biz플랫폼개발팀/SKP <skp.1004683@sk.com>
2023-01-03 20:59:57 +0900

    Initial Commit

---
+++ src/main/java/com/toy/kiyomi/KiyomiApplication.java
@@ -0,0 +1,13 @@
+package com.toy.kiyomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiyomiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiyomiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/kiyomi/KiyomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.kiyomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiyomiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
